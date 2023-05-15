1e047e74aae0d2deedb80a58855ee9dbfdc184a7
Kevin Zhao <kzhao0986@gmail.com>
2023-05-15 15:02:24 -0700

    Currently broken

--- expts/large-distributed/entrypoint.sh
+++ expts/large-distributed/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 mode=${1}
-# Run the script in an infinite loop
-echo "Mode: $mode"
-eval "$mode"
+# echo "Mode: $mode"
+# eval "$mode"
+if [ "$mode" == "parsimon-worker" ]; then
+  eval "$mode"
+elif [ "$mode" == "parsimon-manager" ]; then
+  eval sh
+else
+  echo "Invalid ENTRYPOINT_MODE: $ENTRYPOINT_MODE"
+  exit 1
+fi
