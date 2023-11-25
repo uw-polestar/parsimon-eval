b9d50868ed8fdaca7f7a4d227d0e88256064d62a
lichenni <chenningli2019@gmail.com>
2023-11-25 14:06:37 -0500

    fix trace.txt bug

--- run.sh
+++ run.sh
@@ -16,5 +16,8 @@ cargo run --release -- --mixes spec/all.mix.json ns3
 cargo run --release -- --mixes spec/all.mix.json pmn-m
 cargo run --release -- --mixes spec/all.mix.json pmn-mc
 
+python2 run.py --root mix_parsimon --cc dctcp --trace flows --bw 10 --topo topology
+
+python2 run.py --root mix --cc dctcp --trace flow_parsimon --bw 10 --topo fat_parsimon
 
 PATH=$PATH:/data1/lichenni/software/anaconda3/envs/py27/bin
