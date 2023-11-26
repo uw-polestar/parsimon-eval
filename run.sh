6565ba4568e93539bb76e6f42a24d5f58481774c
lichenni <chenningli2019@gmail.com>
2023-11-25 20:09:50 -0500

    add plot script

--- run.sh
+++ run.sh
@@ -16,8 +16,11 @@ cargo run --release -- --mixes spec/all.mix.json ns3
 cargo run --release -- --mixes spec/all.mix.json pmn-m
 cargo run --release -- --mixes spec/all.mix.json pmn-mc
 
-python2 run.py --root mix_parsimon --cc dctcp --trace flows --bw 10 --topo topology
+python run.py --root /data1/lichenni/projects/flow_simulation/parsimon-eval/expts/fig_8/data/25/ns3 --cc dctcp --trace flows --bw 10 --topo topology --fwin 18000 --base_rtt 14400
 
 python2 run.py --root mix --cc dctcp --trace flow_parsimon --bw 10 --topo fat_parsimon
 
 PATH=$PATH:/data1/lichenni/software/anaconda3/envs/py27/bin
+
+
+CC='gcc-5' CXX='g++-5' ./waf configure --build-profile=optimized
