b6511b7bb4de7ccadedfb2d849d9202d131a4cdd
lichenni <chenningli2019@gmail.com>
2023-12-29 14:34:11 -0500

    wrap up before meeting

--- run_rm.sh
+++ run_rm.sh
@@ -10,9 +10,9 @@ for shard in {0..191..1}
 do
     # COMMAND="rm -rf /data2/lichenni/data_10m/${shard}/mlsys_s2_bt50"
     # COMMAND="mv ${OUTPUT_PATH}/${shard}/mlsys ${OUTPUT_PATH}/${shard}/mlsys_s2_bt100"
-    COMMAND="mv ${OUTPUT_PATH}/${shard}/mlsys_s2_bt100 /data2/lichenni/data_10m/${shard}/"
-    # COMMAND="mv /data2/lichenni/data_10m/${shard}/mlsys_s2_bt50_fast /data2/lichenni/data_10m/${shard}/mlsys_s2_bt50"
-    # COMMAND="mkdir /data2/lichenni/data_10m/${shard}"
+    # COMMAND="mv ${OUTPUT_PATH}/${shard}/mlsys_s2_bt100 /data2/lichenni/data_10m/${shard}/"
+    COMMAND="mv /data2/lichenni/data_10m_/${shard}/* /data2/lichenni/data_10m/${shard}/mlsys_s2_bt100"
+    # COMMAND="mkdir /data2/lichenni/data_10m/${shard}/mlsys_s2_bt100"
     # COMMAND="rm ${OUTPUT_PATH}/${shard}/ns3-path-one/flows.txt"
     echo "$COMMAND">>"$cur_dir/rm.log"
     ${COMMAND}>>"$cur_dir/rm.log"
