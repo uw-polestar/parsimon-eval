6b7e722a936b33c82ffe4cd3b01cdaba69b77ef8
lichenni <chenningli2019@gmail.com>
2024-01-16 22:31:10 -0500

    upd

--- expts/fig_8/analysis/sensitivity_path_mlsys.py
+++ expts/fig_8/analysis/sensitivity_path_mlsys.py
@@ -31,7 +31,8 @@ N_FLOWS_PER_PATH=NR_INTEPOLATE*4
 enable_sample_per_path=False
 sample_per_path_str="_samp" if enable_sample_per_path else "_nosamp"
 # mlsys_dir_list=["mlsys_bdp_bt10_l30"]
-mlsys_dir_list=["mlsys_0114_const"]
+# mlsys_dir_list=["mlsys_0114_const"]
+mlsys_dir_list=["mlsys_0114"]
 legend_list=['ns3','pmn-m',"mlsys"]
 
 for N_FLOW_THRESHOLD in N_FLOW_THRESHOLD_LIST:
