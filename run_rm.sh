8f65c667bc08953b7bd3e5595d2c7cc5842a5576
lichenni <chenningli2019@gmail.com>
2024-01-21 16:28:35 -0500

    finish motivation

--- run_rm.sh
+++ run_rm.sh
@@ -8,12 +8,12 @@ OUTPUT_PATH='/data1/lichenni/projects/flow_simulation/parsimon-eval/expts/fig_8/
 for shard in {0..191..1}
 # for shard in 0
 do
-    COMMAND="rm -rf ${OUTPUT_PATH}/${shard}/ns3-param"
+    # COMMAND="rm -rf ${OUTPUT_PATH}/${shard}/ns3-param"
     # COMMAND="mv ${OUTPUT_PATH}/${shard}/mlsys ${OUTPUT_PATH}/${shard}/mlsys_bt1_p100_bdp"
     # COMMAND="mv ${OUTPUT_PATH}/${shard}/mlsys_bt100 /data2/lichenni/data_10m/${shard}/"
     # COMMAND="mv /data2/lichenni/data_10m_/${shard}/* /data2/lichenni/data_10m/${shard}/mlsys_s2_bt100"
     # COMMAND="mkdir /data2/lichenni/data_10m/${shard}/mlsys_s2_bt100"
-    # COMMAND="rm ${OUTPUT_PATH}/${shard}/ns3-path-one/flows.txt"
+    # COMMAND="rm ${OUTPUT_PATH}/${shard}/ns3/flows.txt"
     echo "$COMMAND">>"$cur_dir/rm.log"
     ${COMMAND}>>"$cur_dir/rm.log"
     echo "">>"$cur_dir/rm.log"
