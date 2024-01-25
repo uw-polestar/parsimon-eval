ff3f5fc771a4c928250705cc6ea8eca64c9864ee
lichenni <chenningli2019@gmail.com>
2024-01-24 22:59:00 -0500

    go home

--- expts/fig_8/spec/gen_spec.py
+++ expts/fig_8/spec/gen_spec.py
@@ -11,23 +11,23 @@ def filter_json(json_file, output_file):
     filtered_data = [item for item_idx, item in enumerate(data) if not os.path.exists(f"/data1/lichenni/projects/flow_simulation/parsimon-eval/expts/fig_8/data/{item_idx}/ns3-param/records.csv")]
     print(len(filtered_data))
     # Export the filtered data to a new JSON file
-    with open(output_file, 'w') as f:
-        json.dump(filtered_data, f, indent=2)
+    # with open(output_file, 'w') as f:
+    #     json.dump(filtered_data, f, indent=2)
 
-def sub_json(json_file, json_file_sub, output_file):
-    # Read the JSON file
-    with open(json_file, 'r') as f:
-        data = json.load(f)
+# def sub_json(json_file, json_file_sub, output_file):
+#     # Read the JSON file
+#     with open(json_file, 'r') as f:
+#         data = json.load(f)
     
-    with open(json_file_sub, 'r') as f:
-        data_sub = json.load(f)
-    filter_list = set([x['id'] for x in data_sub])
-    # Filter based on the existence of corresponding files
-    filtered_data = [item for item in data if not item['id'] in filter_list]
-    print(len(filtered_data))
-    # Export the filtered data to a new JSON file
-    with open(output_file, 'w') as f:
-        json.dump(filtered_data, f, indent=2)
+#     with open(json_file_sub, 'r') as f:
+#         data_sub = json.load(f)
+#     filter_list = set([x['id'] for x in data_sub])
+#     # Filter based on the existence of corresponding files
+#     filtered_data = [item for item in data if not item['id'] in filter_list]
+#     print(len(filtered_data))
+#     # Export the filtered data to a new JSON file
+#     with open(output_file, 'w') as f:
+#         json.dump(filtered_data, f, indent=2)
     
 # Example usage
 json_file_path = 'all.mix.json'  # Replace with your JSON file path
