a93590b65690a84f642f239488626f3fa98f4252
lichenni <chenningli2019@gmail.com>
2024-03-31 22:20:33 -0400

    upd

--- run.sh
+++ run.sh
@@ -1,39 +1,21 @@
 cargo run --release -- help
 
 # fig-7
-cargo run --release -- --mix spec/1.mix.json ns3
-cargo run --release -- --mix spec/1.mix.json pmn-m
-cargo run --release -- --mix spec/1.mix.json pmn-mc
-cargo run --release -- --mix spec/1.mix.json pmn-path
-cargo run --release -- --mix spec/2_config.mix.json ns3
+cargo run --release -- --mix spec/0_config.mix_b_03_5.json ns3
+cargo run --release -- --mix spec/0_config.mix_b_03_5.json pmn-m
+cargo run --release -- --mix spec/0_config.mix_b_03_5.json mlsys
 
 # fig-8
-cargo run --release -- --root=./data --mixes spec/0.mix.json ns3-config
-cargo run --release -- --root=./data --mixes spec/0.mix.json pmn-m
-cargo run --release -- --root=./data --mixes spec/0.mix.json mlsys
+cargo run --release -- --root=./data --mixes spec/all_dctcp.mix.json ns3-config
+cargo run --release -- --root=./data --mixes spec/all_dctcp.mix.json pmn-m
+cargo run --release -- --root=./data --mixes spec/all_dctcp.mix.json mlsys
 
 cargo flamegraph -- --mixes spec/0.mix.json mlsys
 
 # counterfactual search
-cargo run --release -- --root=./data_dctcp --mixes spec/all_counterfactual_dctcp.mix.json ns3-config
-cargo run --release -- --root=./data_hpcc --mixes spec/all_counterfactual_hpcc.mix.json ns3-config
-
+cargo run --release -- --root=./data --mixes spec/all_counterfactual.mix.json ns3-config
+cargo run --release -- --root=./data --mixes spec/all_counterfactual.mix.json mlsys
 
 # fig-8 gen json, remember to change the random seed in m3/parsimon-eval/expts/fig_8/src/bin/gen_mixes.rs
 cargo run --bin gen_mixes -- --input spec/all_dctcp.mixspace.json --count 192 --output spec/all_dctcp.mix.json
-
 cargo run --bin gen_mixes -- --input spec/all_counterfactual.mixspace.json --count 192 --output spec/all_counterfactual.mix.json
-
-cargo run --bin gen_mixes -- --input spec/all_counterfactual.mixspace_dctcp.json --count 25 --output spec/all_counterfactual_dctcp.mix.json
-
-cargo run --bin gen_mixes -- --input spec/all_counterfactual.mixspace_hpcc.json --count 25 --output spec/all_counterfactual_hpcc.mix.json
-
-# test ns3 from parsimon-eval
-cd /data1/lichenni/projects/flow_simulation/parsimon-eval/expts/fig_8/src
-cargo test --lib ns3
-
-# test network from parsimon-core
-cd /data1/lichenni/projects/flow_simulation/parsimon/crates/parsimon-core/src
-cargo test --lib network
-
-cargo update -p parsimon
