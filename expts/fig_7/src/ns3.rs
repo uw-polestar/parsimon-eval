883382b382975af4f3c8fb94de883f0c3a47e96c
lichenni <chenningli2019@gmail.com>
2024-11-21 02:00:49 -0500

    upd fig 7

--- expts/fig_7/src/ns3.rs
+++ expts/fig_7/src/ns3.rs
@@ -114,7 +114,7 @@ impl Ns3Simulation {
         let cc = self.cc_kind.as_str();
         let param_1 = self.param_1;
         let param_2 = self.param_2;
-        let enable_tr = 1;
+        let enable_tr = 0;
         // let command_sim = format!(
         //     "python2 run.py --root {data_dir} --base_rtt {base_rtt} \
         //     --topo topology --trace flows --bw 10 --bfsz {bfsz} --fwin {window} --enable_pfc {enable_pfc} --cc {cc} --param_1 {param_1} --param_2 {param_2} \
@@ -126,7 +126,7 @@ impl Ns3Simulation {
         );
         
         let command_post = format!(
-            "python run_m4_post.py -p topology_flows --output_dir {data_dir} --random_seed {mix_id} --cc {cc} --shard_cc 0 --enable_tr {enable_tr} --max_inflight_flows 0 \
+            "python run_m4_post.py -p topology_flows --output_dir {data_dir} --random_seed 0 --cc {cc} --shard_cc 0 --enable_tr {enable_tr} --max_inflight_flows 0 \
             > {data_dir}/log_post.txt 2>&1"
         );
 
