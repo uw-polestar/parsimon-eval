e05d513cfae258493bbe79d7c9dba9dd8e267c5e
lichenni <chenningli2019@gmail.com>
2025-01-25 15:17:31 -0500

    upd large traffic pattern

--- expts/fig_7/src/ns3.rs
+++ expts/fig_7/src/ns3.rs
@@ -150,16 +150,16 @@ impl Ns3Simulation {
         ); 
         // println!("{command_sim}");
         // Execute the command in a child process.
-        //let _output = Command::new("sh")
-        //    .arg("-c")
-            // .arg(format!("cd {ns3_dir}; {command_sim}; rm {data_dir}/flows.txt"))
-        //    .arg(format!("cd {ns3_dir};{command_sim}; {command_post};"))
-            // .arg(format!("cd {ns3_dir}; {command_post}"))
-        //    .output()?;
-        let _output_flowsim = Command::new("sh")
+        let _output = Command::new("sh")
            .arg("-c")
-           .arg(format!("{command_flowsim_pre}; {command_flowsim};"))
+            // .arg(format!("cd {ns3_dir}; {command_sim}; rm {data_dir}/flows.txt"))
+           .arg(format!("cd {ns3_dir};{command_sim}; {command_post};"))
+            // .arg(format!("cd {ns3_dir}; {command_post}"))
            .output()?;
+        // let _output_flowsim = Command::new("sh")
+        //    .arg("-c")
+        //    .arg(format!("{command_flowsim_pre}; {command_flowsim};"))
+        //    .output()?;
         // let _output_m4 = Command::new("sh")
         //     .arg("-c")
         //     .arg(format!("{command_flowsim_pre}; {command_m4};"))
